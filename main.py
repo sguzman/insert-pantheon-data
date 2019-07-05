ea596def1297e6135e03c26a878fffdd40355a3b
Salvador Guzman <guzmansalv@gmail.com>
2019-07-05 14:24:42 -0700

    main stub

---
+++ main.py
@@ -0,0 +1,22 @@
+import atexit
+import json
+import psycopg2
+
+
+def con() -> psycopg2:
+    conn: psycopg2 = \
+        psycopg2.connect(user='salvadorguzman', password='', host='127.0.0.1', port='5432', database='personal')
+
+    def clean_up() -> None:
+        conn.close()
+        print('Closing connection', conn)
+
+    return conn
+
+
+def main() -> None:
+    print('hello')
+
+
+if __name__ == '__main__':
+    main()
